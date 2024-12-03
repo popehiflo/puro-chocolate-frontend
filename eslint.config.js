6d42a5d57d86c125c472a9aaff78496700340757
Pool Petter Hijuela Florian <poolpetterhijuel@hotmail.com>
2024-12-03 11:45:44 -0500

    fix: update files with 'npm run lint' & 'npm run lint:fix'

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettier from 'eslint-config-prettier';
 
 export default [
   { ignores: ['dist'] },
@@ -28,14 +29,15 @@ export default [
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
+      ...prettier.rules,
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
-      'semi': ['error', 'always'], // Requerir punto y coma
-      'quotes': ['error', 'single'], // Usar comillas simples
-      'react/prop-types': 'off' // Desactivar validación de prop-types
+      semi: ['error', 'always'], // Requerir punto y coma
+      quotes: ['error', 'single'], // Usar comillas simples
+      'react/prop-types': 'off', // Desactivar validación de prop-types
     },
   },
-]
+];
